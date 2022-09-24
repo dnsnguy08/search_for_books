34b8eeeb0753378bdb1ef156d10008b6142b39d7
Dennis Nguyen <dnsnguy08@gmail.com>
2022-09-23 22:53:49 -0700

    fixed lint check errors

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,7 @@ const typeDefs = gql`
     password: String
     savedBooks: [Book]
   }
+
   type Book {
     _id: ID!
     bookId: String
@@ -17,13 +18,16 @@ const typeDefs = gql`
     link: String
     title: String
   }
+
   type Auth {
       token: ID!
       user: User
   	}
+
   type Query {
     user: User
   }
+
   type Mutation {
     createUser(username: String!, email: String!, password: String!): Auth
     saveBook(authors: [String], description: String!, bookId: String!, image: String, link: String, title: String!): User
